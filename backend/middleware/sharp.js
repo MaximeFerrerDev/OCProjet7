8c4145765617f9d8ef5ae06a6e25e091f20813c0
MaximeFerrerDev <maximeferrer.dev@gmail.com>
2024-03-16 11:53:58 +0100

    Creating the folder to store images if not found

--- backend/middleware/sharp.js
+++ backend/middleware/sharp.js
@@ -1,4 +1,5 @@
 const sharp = require('sharp')
+const fs = require('fs')
 
 module.exports = (req, res, next) => {
     // Getting the image and the name from the buffer
@@ -12,6 +13,13 @@ module.exports = (req, res, next) => {
 
     // Saving the reference in "filename" to be used later
     req.file.filename = ref
+
+    // Checking if the /images folder exists, if not creating it
+    fs.access('./images', (error) => {
+        if (error) {
+            fs.mkdirSync('./images')
+        }
+    })
 
     // Saving the converted image
     sharp(buffer)
